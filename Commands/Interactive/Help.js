aefa0d900d79f1a6b308185530aa38595bf5f071
CrabPulsar <txryan1@gmail.com>
2021-05-08 21:26:25 +0000

    Update Help.js

--- Commands/Interactive/Help.js
+++ Commands/Interactive/Help.js
@@ -1,35 +1,29 @@
-const colors = require("../../colors.json");
-const desc = require("../../index.js");
-const prefix = require("../../index.js");
+const index = require("../../index.js");
+const random = require('../../Helper Functions/Random Color');
 module.exports.run = async (bot, message, args) =>{
     if (!args[0]){
         return message.reply("What command do you need help with? Try using jssc to see the list of recognized commands.")
     }
-    var initialLength = prefix.currentPrefix.length
+    var initialLength = index.currentPrefix.length
     var input = `${message.content}`.toLowerCase().slice(initialLength + 4).trim()
-    if (!desc.fileDescription.get(input)){
-        return message.reply("Unrecognized command!")
+    x = 0
+    y = 0
+    if (!index.namedFileDescription.get(input)){
+        x = x + 1
+    }
+    if (!index.aliasFileDescription.get(input)){
+        y = y + 1
+    }
+    if (x && y >= 1){
+        return message.reply("Unrecognized command!");
+    } else if (x >= 1){
+        var description = index.aliasFileDescription.get(input);
+    } else if (y >= 1){
+        var description = index.namedFileDescription.get(input);
     }
     const embed = {};
-    number = 14
-    var random = Math.floor(Math.random() * (number - 1 + 1)) + 1;
-    switch(random){
-        case 1: embed.color = `${colors["black"]}`; break;
-        case 2: embed.color = `${colors["white"]}`; break;
-        case 3: embed.color = `${colors["gray"]}`; break;
-        case 4: embed.color = `${colors["brown"]}`; break;
-        case 5: embed.color = `${colors["gold"]}`; break;
-        case 6: embed.color = `${colors["green"]}`; break;
-        case 7: embed.color = `${colors["red"]}`; break;
-        case 8: embed.color = `${colors["blue"]}`; break;
-        case 9: embed.color = `${colors["purple"]}`; break;
-        case 10: embed.color = `${colors["yellow"]}`; break;
-        case 11: embed.color = `${colors["orange"]}`; break;
-        case 12: embed.color = `${colors["hotpink"]}`; break;
-        case 13: embed.color = `${colors["cream"]}`; break;
-        case 14: embed.color = `${colors["kewl green"]}`; break;
-    }
-    embed.fields = [{name: `**${input.toUpperCase()}**`, value: desc.fileDescription.get(input)}];
+    embed.color = `${random.randomColor()}`;
+    embed.fields = [{name: `**${input.toUpperCase()}**`, value: description}];
     embed.timestamp = new Date(),
     embed.footer = {text: `Requested by ${message.author.username}`, icon_url: message.author.displayAvatarURL({dynamic: true})}
     message.channel.send({embed: embed});

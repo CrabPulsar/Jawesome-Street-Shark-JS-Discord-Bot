493e967f06bd840dd07ea35ebf0cbc960f767ee4
CrabPulsar <txryan1@gmail.com>
2022-02-18 23:51:37 +0000

    Update Commands.js

--- Commands/Admin/Commands.js
+++ Commands/Admin/Commands.js
@@ -3,27 +3,35 @@ module.exports.run = async (bot, message, args) =>{
 const Comembed = {
             title: "Commands",
             fields: [
-              {name: "**SFW Anime Commands:**",
-              value: "• Hug\n• Kiss\n• Neko\n• Nekogif\n• Rananime {r}\n• Slap\n• Tickle\n• Waifu {w}",
+              {name: "**Admin:**",
+              value: "• Botinfo {bot}\n• Commands {c}\n• Delete (#<100) {d}\n• Mute\n• Ping\n• Roles\n• ServerInfo {si}\n• Unmute\n• Uptime",
               inline: true},
-              {name: "**NSFW Anime Commands:**",
-              value: "• Anal\n• Feet\n• Feetgif\n• Rananimensfw {rn}\n• Spank",
-              inline: true},
-              {name: "**Management:**",
-              value: "• Commands {c}\n• Delete (#<100) {d}\n• Mute\n• Ping\n• Roles\n• Unmute\n• Uptime",
-              inline: true},
+
               {name: "**Animals:**",
               value: "• Cat\n• Dog\n• Goose",
               inline: true},
+
               {name: "**Gifs and Videos:**",
               value: "• Assad\n• Cyberpunk {2077}\n• Dumb\n• Gif\n• Jawesome\n• Justin\n• NSFW\n• Politics\n• Rules\n• SecretSide {ss}\n• Today\n• Turkey\n• Vince\n• Zeb",
               inline: true},
+
               {name: "**Interactive:**",
               value: "• Avatar\n• Color\n• Emotify\n• Gelbooru {g}\n• Gray\n• Hello\n• Help\n• Images {i}\n• Spam\n• Userinfo {ui}",
               inline: true},
+
               {name: "**Math:**",
               value: "• Factorial {fac}\n• Math {m}",
-              inline: true}
+              inline: true},
+
+              {name: "**Music:**",
+              value: "• Clip\n• Clips\n• Invite\n• Loop {l}\n• Lyrics {ly}\n• Move {mv}\n• Np\n• Pause\n• Play {p}\n• Playlist {pl}\n• Pruning\n• Queue {q}\n• Remove {rm}\n• Resume {r}\n• Search\n• Shuffle\n• Skip {s}\n• Skipto {st}\n• Stop\n• Volume {v}",
+              inline: true
+              },
+
+              {name: "**SFW Anime Commands:**",
+              value: "• Hug\n• Kiss\n• Neko\n• Nekogif\n• Rananime {r}\n• Slap\n• Tickle\n• Toradora {vsalt}\n• Waifu {w}",
+              inline: true},
+              
             ],
             timestamp: new Date(),
             footer: {
@@ -34,7 +42,7 @@ const Comembed = {
       Comembed.color = `${random.randomColor()}`;
       message.channel.send({embed: Comembed});
       message.delete();
-    }
+}
 
 module.exports.help = {
     name: "commands",
